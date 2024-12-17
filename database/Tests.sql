bf0cdad236fe6368df72d03897c380232bdebb8e
coolrunner1 <waurttttt@gmail.com>
2024-12-17 13:11:07 +0300

    added new tests

--- database/Tests.sql
+++ database/Tests.sql
@@ -1,25 +1,47 @@
+SELECT * FROM Category;
+
+SELECT * FROM Client;
+
+SELECT * FROM Role;
+
+SELECT * FROM Supplier;
+
+SELECT * FROM ClientOrder;
+
+SELECT * FROM Item;
+
+SELECT * FROM UserProfile;
+
 SELECT * FROM OrderProduct;
+
+SELECT * FROM SupplierItem;
+
+SELECT * FROM OrderReturn;
+
+SELECT * FROM Item WHERE model ILIKE '%lap%' ORDER BY model;
+
+SELECT * FROM Item JOIN Category ON Item.category_id = Category.category_id
+	WHERE Item.category_id = 1 OR Category.parent_id = 1;
+
+SELECT OrderReturn.order_return_id, OrderReturn.quantity, reason, description,
+            OrderReturn.status, return_date, Item.item_id, model, image_path, manufacturer, unit_price, OrderProduct.order_id
+            FROM OrderReturn JOIN OrderProduct ON OrderReturn.order_product_id = OrderProduct.order_product_id
+            JOIN Item ON OrderProduct.item_id = Item.item_id WHERE OrderProduct.order_id = 14;
+
+SELECT * FROM ClientOrder WHERE CAST(order_id AS VARCHAR(255)) LIKE '%1%';
+
+SELECT * FROM Item ORDER BY unit_price, model;
 
 UPDATE Item
 	   SET units_in_stock = 0
 	   WHERE item_id = 4;
 
-SELECT * FROM Item;
-SELECT last_value FROM order_id_seq;
 UPDATE ClientOrder SET status = 'Delivered' WHERE order_id = 3;
 
 UPDATE ClientOrder SET status = 'Delivered' WHERE order_id = 1;
-SELECT OrderReturn.order_return_id, OrderReturn.quantity, reason, description, OrderReturn.status, return_date, Item.item_id, model, image_path, manufacturer, unit_price FROM OrderReturn JOIN OrderProduct ON OrderReturn.order_product_id = OrderProduct.order_product_id JOIN Item ON OrderProduct.item_id = Item.item_id WHERE OrderProduct.order_id = 1;
-SELECT UNNEST(enum_range(null, null::return_status));
 
 UPDATE OrderProduct SET quantity = 5 WHERE order_id = 1;
 
 UPDATE ClientOrder SET status = 'Delivered';
-
-SELECT Item.item_id, Item.model, Item.image_path, Item.manufacturer, Item.unit_price, OrderProduct.quantity, OrderProduct.returned_units
-FROM Item JOIN OrderProduct ON Item.item_id = OrderProduct.item_id WHERE OrderProduct.order_id = 3;
-SELECT * FROM UserProfile;
-
-SELECT CAST(order_id AS VARCHAR(255)) FROM ClientOrder;
 
 UPDATE ClientOrder SET status = 'Delivered';
