85d55357f48d213e08a46a6d5424d833147f9f7c
coolrunner1 <waurttttt@gmail.com>
2025-06-20 11:11:08 +0300

    Minor improvements

--- frontend/src/components/Global/TextEditor.tsx
+++ frontend/src/components/Global/TextEditor.tsx
@@ -21,7 +21,7 @@ export const TextEditor = (props: TextEditorProps) => {
                     'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
                 ],
                 toolbar: 'undo redo | blocks | ' +
-                    'bold italic | alignleft aligncenter ' +
+                    'bold italic forecolor | alignleft aligncenter ' +
                     'alignright alignjustify | bullist numlist outdent indent | ' +
                     'removeformat | help',
                 content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
